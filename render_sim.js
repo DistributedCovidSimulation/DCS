a9cd63aff240dfb53c08f658d0e5be448119051f
Edward Li <radioactivehydra@gmail.com>
2021-08-15 09:35:34 -0400

    Final Commit

--- render_sim.js
+++ render_sim.js
@@ -2,6 +2,8 @@ async function start_render() {
     config = JSON.parse(document.getElementById("config").value || "{}");
     const model = workFn({ ...config, render_mode: true });
     
+    const maxTime = 1000;
+
     const canvas = document.getElementById("outputCanvas");
     canvas.width = 400;
     canvas.height = 400;
@@ -10,12 +12,22 @@ async function start_render() {
     const ctx = canvas.getContext("2d");
     ctx.imageSmoothingEnabled= false
 
+    const canvas2 = document.getElementById("outputCanvas2");
+    canvas2.width = maxTime;
+    canvas2.height = 100;
+    const canvas2Width = canvas2.width;
+    const canvas2Height = canvas2.height;
+    const ctx2 = canvas2.getContext("2d");
+    ctx2.imageSmoothingEnabled= false
+    ctx2.clearRect(0, 0, canvas2Width, canvas2Height);
+
+
     ctx.font = "700 14px Consolas";
     ctx.fillStyle = "#0000ff";
 
     // That's how you define the value of a pixel //
-    function drawPixel(canvasData, x, y, r, g, b, a=255) {
-      const index = (Math.floor(x * canvasWidth) + Math.floor(y * canvasHeight) * canvasWidth) * 4;
+    function drawPixel(canvasData, x, y, r, g, b, a=255, width=canvasWidth, height=canvasHeight) {
+      const index = (Math.floor(x * width) + Math.floor(y * height) * width) * 4;
 
       canvasData.data[index + 0] = r;
       canvasData.data[index + 1] = g;
@@ -23,7 +35,6 @@ async function start_render() {
       canvasData.data[index + 3] = a;
     }
 
-    const maxTime = 1000;
     let time = 0;
     const t = setInterval(() => {
       if (time >= maxTime) {
@@ -33,10 +44,22 @@ async function start_render() {
 
       ctx.clearRect(0, 0, canvasWidth, canvasHeight);
       const canvasData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+      const canvas2Data = ctx2.getImageData(0, 0, canvas2Width, canvas2Height);
       model.tick(time);
-      model.tick(time);
-      time += 2;
+      time += 1;
 
+      console.log(time/maxTime, 1 - (model.results[model.results.length - 1].S / 5000))
+      drawPixel(canvas2Data, time/maxTime, 1 - (model.results[model.results.length - 1].S / 5000), 0, 0, 0, 255, canvas2Width, canvas2Height)
+      drawPixel(canvas2Data, time/maxTime, 1 - (model.results[model.results.length - 1].I / 5000), 200, 200, 200, 255, canvas2Width, canvas2Height)
+      drawPixel(canvas2Data, time/maxTime, 1 - (model.results[model.results.length - 1].R / 5000), 255, 0, 0, 255, canvas2Width, canvas2Height)
+
+      model.tick(time);
+      time += 1;
+
+      drawPixel(canvas2Data, time/maxTime, 1 - (model.results[model.results.length - 1].S / 5000), 0, 0, 0, 255, canvas2Width, canvas2Height)
+      drawPixel(canvas2Data, time/maxTime, 1 - (model.results[model.results.length - 1].I / 5000), 200, 200, 200, 255, canvas2Width, canvas2Height)
+      drawPixel(canvas2Data, time/maxTime, 1 - (model.results[model.results.length - 1].R / 5000), 255, 0, 0, 255, canvas2Width, canvas2Height)
+      
       for (person of model.people) {
         if (person.status === 0) {
           drawPixel(canvasData, person.x, person.y, 0, 0, 0);
@@ -46,7 +69,9 @@ async function start_render() {
           drawPixel(canvasData, person.x, person.y, 255, 0, 0);
         }
       }
+
       ctx.putImageData(canvasData, 0, 0);
+      ctx2.putImageData(canvas2Data, 0, 0);
   
       ctx.fillText(`Distributed Covid Simulation [SIM]`, 10, 20);
       ctx.fillText(`Day:         ${time}`, 10, 35);
